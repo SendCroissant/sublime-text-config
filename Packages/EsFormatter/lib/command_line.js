c1c1aa8bdcdad26923cbfb9c8266a6170059a90d
Rostislav <rostislav@adkernel.com>
2015-03-11 14:43:54 +0200

    Initial commit

---
+++ Packages/EsFormatter/lib/command_line.js
@@ -0,0 +1,33 @@
+;(function() {
+  var buf = '';
+  var esformatter;
+  try {
+    // Try to require the global esformatter
+    esformatter = require('esformatter');
+  } catch (ex) {
+    // Use the bundle version
+    esformatter = require('formatter');
+  }
+  var options = JSON.parse(process.argv[2] || "{}");
+
+
+  process.stdin.on('data', function(chunk) {
+    buf += chunk;
+  });
+
+  process.stdin.on('end', function() {
+    try {
+      var result = esformatter.format(buf, options);
+      process.stdout.write(result);
+      setTimeout(function() {
+        // When opening many threads, the buffer might be slow and send back
+        // an empty string. Wait a little bit for the drain
+        process.exit(0);
+      }, 15);
+    } catch (ex) {
+      throw "__EX-MESSAGE>_Unable to format_<EX-MESSAGE__";
+    }
+  });
+
+  process.stdin.resume();
+})();
